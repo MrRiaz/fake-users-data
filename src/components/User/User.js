db7ca4eee4d5fe5a93651b0af00cec0181da242f
MrRiaz <monsurrahmanriaz@gmail.com>
2020-08-26 00:17:49 +0600

    done

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const User = (props) => {
-    const {name, username, email, phone, img} = props.user;
+    const {name, username, email, phone, img, salary} = props.user;
     
     return (
         <div className = 'user'>
@@ -13,9 +13,10 @@ const User = (props) => {
             </div>
             <div className="user-name">
                 <h3>Name: {name}</h3>
-                <p> Uname: {username} </p>
-                <p> Email: {email} </p>
-                <p> Phone: {phone} </p>
+                <p><small>Uname: {username}</small></p>
+                <p><small>Email: {email}</small> </p>
+                <p><small>Salary: {salary}</small></p>
+                <p><small>Phone: {phone}</small> </p>
                 <button onClick = { () => props.handleDetailsId(props.user)}><FontAwesomeIcon icon={faPlus} /> more about</button>
             </div>
         </div>
